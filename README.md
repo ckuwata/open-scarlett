759ab9e80cb8385ad3b3ea856ee82c058bbacee3
ckuwata <chikara.kuwata@gmail.com>
2019-11-15 03:15:01 +0900

    Added usage, refactored

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # open-scarlett
 Unofficial Focusrite Scarlett controller
+
+## Requirement
+
+* pyusb
+
+## Usage
+
+```
+scarlett18i20g3.py [-h] {air,impedance,pad} ...
+
+positional arguments:
+  {air,impedance,pad}  sub-command help
+    air                switch analog input's AIR mode
+    impedance          switch analog input's impedance
+    pad                switch analog input's PAD
+
+optional arguments:
+  -h, --help           show this help message and exit
+
+```
--- src/scarlett18i20g3.py
+++ src/scarlett18i20g3.py
@@ -1,26 +1,24 @@
+#!/usr/bin/python3
 import usb.core
 import usb.backend.libusb1
 from ctypes import c_void_p, c_int
 backend = usb.backend.libusb1.get_backend()
 
-from usb.core import USBError
 from usb.util import CTRL_IN, CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE, build_request_type
-from usb.legacy import DeviceHandle
 from usb.control import get_status
 
 VENDOR_ID = 0x1235
 PRODUCT_ID = 0x8215
 
-LINE_INST = [0x7C, 0x7D]
+IMPEDANCE = [0x7C, 0x7D]
 PAD = list(range(0x84, 0x8C))
 AIR = list(range(0x8C, 0x94))
 class Scarlett18i20G3:
   def __init__(self):
     self._device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID, backend=backend)
     self._sequence = [1, 0] # sequence number is 2byte, [lower byte, higher byte]
-    if self._device.is_kernel_driver_active(1):
+    if self._device is not None and self._device.is_kernel_driver_active(1):
       self._device.detach_kernel_driver(1)
-      #self._device.set_configuration()
 
   def validate(self):
     return self._device is not None
@@ -32,7 +30,6 @@ class Scarlett18i20G3:
     self._device.ctrl_transfer(0x21, 0x02, 0x00, 0x03,
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-    #self._device.read(0x83, 3, 1)
     ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
 
 
@@ -47,56 +44,53 @@ class Scarlett18i20G3:
       if self._sequence[1] > 0xFF:
         self.initialize()
 
-  def send_air_change(self, input, status):
+  def send_air_switch(self, input, status):
     self._ctrl_transfer([0x01, 0x00, 0x80, 0x00, 0x09, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     AIR[input-1], 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
     status])
     ret = (bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010)))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
 
     self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00])
     ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
-    self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
-    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00])
-    ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
 
-  def send_line_change(self, input, status):
+  def send_impedance_switch(self, input, status):
     self._ctrl_transfer([0x01, 0x00, 0x80, 0x00, 0x09, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    LINE_INST[input-1], 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    IMPEDANCE[input-1], 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
     status])
     ret = (bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010)))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
 
     self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00])
     ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
-    self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
-    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00])
-    ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
 
-  def send_pad_change(self, input, status):
+  def send_pad_switch(self, input, status):
     self._ctrl_transfer([0x01, 0x00, 0x80, 0x00, 0x09, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     PAD[input-1], 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
     status])
     ret = (bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010)))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
 
     self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00])
     ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
-    self._ctrl_transfer([0x02, 0x00, 0x80, 0x00, 0x04, 0x00],
-    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00])
-    ret = bytearray(self._device.ctrl_transfer(0xA1, 0x03, 0x00, 0x03, 0x0010))
-    print(' '.join(map(lambda x: '{0:0{1}x}'.format(x, 2), ret)))
+    if self.is_error(ret):
+      return
+
+  def is_error(self, ret_code):
+    return all(map(lambda x:x==0, ret_code[4:]))
 
   def __str__(self):
     return str(self._device)
@@ -106,35 +100,36 @@ if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser()
   subparsers = parser.add_subparsers(help='sub-command help', dest='name')
-  parser_air = subparsers.add_parser('air', help="change analog input's AIR mode")
+  parser_air = subparsers.add_parser('air', help="switch analog input's AIR mode")
   parser_air.add_argument('input', type=int, help='analog input [1-8]')
   parser_air.add_argument('status', type=int, help='0=AIR Off, 1=AIR On')
 
-  parser_line = subparsers.add_parser('line', help="change analog input's LINE/INST")
-  parser_line.add_argument('input', type=int, help='analog input [1-2]')
-  parser_line.add_argument('status', type=int, help='0=LINE, 1=INST')
+  parser_impedance = subparsers.add_parser('impedance', help="switch analog input's impedance")
+  parser_impedance.add_argument('input', type=int, help='analog input [1-2]')
+  parser_impedance.add_argument('status', type=int, help='0=LINE, 1=INST')
 
-  parser_pad = subparsers.add_parser('pad', help="change analog input's PAD")
+  parser_pad = subparsers.add_parser('pad', help="switch analog input's PAD")
   parser_pad.add_argument('input', type=int, help='analog input [1-8]')
   parser_pad.add_argument('status', type=int, help='0=Off, 1=On')
-
+  arg = parser.parse_args(sys.argv[1:])
+  if arg.name is None:
+    parser.print_help()
+    sys.exit(1)
 
   scarlett = Scarlett18i20G3()
   if not scarlett.validate():
     print('Device is not connected')
     sys.exit(1)
-  else:
-    print('Device is connected.')
   scarlett.initialize()
-  arg = parser.parse_args(sys.argv[1:])
+
   if arg.name == 'air':
-    scarlett.send_air_change(arg.input, arg.status)
-  elif arg.name == 'line':
+    scarlett.send_air_switch(arg.input, arg.status)
+  elif arg.name == 'impedance':
     if arg.input > 2:
       print('Only analog input 1-2 is available')
       sys.exit(1)
-    scarlett.send_line_change(arg.input, arg.status)
+    scarlett.send_impedance_switch(arg.input, arg.status)
   elif arg.name == 'pad':
-    scarlett.send_pad_change(arg.input, arg.status)
+    scarlett.send_pad_switch(arg.input, arg.status)
   else:
     parser.print_help()
